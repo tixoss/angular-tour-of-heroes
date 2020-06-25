4cbc6940d4e8ca62bf738eb49adf36694f0822af
stas.fedorov <stas.fedorov@waveaccess.ru>
2020-06-25 21:35:54 +0300

    Displaying heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
+  //styles: '',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
@@ -12,6 +14,8 @@ export class HeroesComponent implements OnInit {
     id: 1,
     name: 'Windstorm'
   };
+
+  heroes = HEROES;
 
   constructor() { }
 
